00fe2137eb6e19dd115d5c3d2cecfc70b4dafabe
David Zarinski <david.zarinski@gmail.com>
2023-01-31 22:50:15 -0700

    Added Stream and Batch mode selection and validation

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -4,10 +4,14 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
+  HStack,
+  Input,
   InputGroup,
   InputRightElement,
   NumberInput,
   NumberInputField,
+  Radio,
+  RadioGroup,
   VStack,
 } from '@chakra-ui/react';
 import { Field, Form, Formik } from 'formik';
@@ -17,6 +21,7 @@ function Settings() {
   const validate = values => {
     const errors = {};
 
+    // repeatingLoglines validation
     if (
       isNaN(values.repeatingLoglines) ||
       Number(values.repeatingLoglines) < 0 ||
@@ -25,13 +30,37 @@ function Settings() {
       errors.repeatingLoglines = 'Must be between 0% and 100%';
     }
 
+    // mode validation
+    if (values.mode === 'Stream') {
+      // stream mode validation
+      if (values.stream.streamAddress === '') {
+        errors.stream = {};
+        errors.stream.streamAddress = 'Required';
+      }
+    } else if (values.mode === 'Batch') {
+      // batch mode validation
+      if (isNaN(values.batch.numberOfLogs)) {
+        errors.batch = {};
+        errors.batch.numberOfLogs = 'Must be a number';
+      } else if (Number(values.batch.numberOfLogs) < 1) {
+        errors.batch = {};
+        errors.batch.numberOfLogs = 'Must be more than 0';
+      } else if (Number(values.batch.numberOfLogs) > 1000000000) {
+        errors.batch = {};
+        errors.batch.numberOfLogs =
+          'Must be a number equal or below 1,000,000,000';
+      }
+    } else {
+      errors.mode = 'Invalid mode selected';
+    }
+
     return errors;
   };
 
   return (
     <Formik
       initialValues={{
-        repeatingLoglines: '0',
+        repeatingLoglines: 0,
         fields: {
           includeTimeStamp: true,
           includeProcessingTime: true,
@@ -46,9 +75,21 @@ function Settings() {
           includeAdware: false,
           includeRansom: false,
         },
+        mode: 'Stream',
+        stream: {
+          saveLogs: false,
+          streamAddress: '',
+        },
+        batch: {
+          numberOfLogs: 0,
+        },
       }}
       validate={validate}
       onSubmit={(values, actions) => {
+        // round numberOfLogs to nearest integer
+        values.batch.numberOfLogs = Math.round(values.batch.numberOfLogs);
+
+        // display json output
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
           actions.setSubmitting(false);
@@ -151,6 +192,75 @@ function Settings() {
                 </Field>
               </VStack>
             </FormControl>
+            <Field name="mode">
+              {({ field, meta }) => {
+                const { onChange, ...rest } = field;
+                return (
+                  <FormControl id="mode" isInvalid={meta.touched && meta.error}>
+                    <FormLabel htmlFor="mode">Select Mode</FormLabel>
+                    <RadioGroup {...rest} id="mode">
+                      <HStack spacing="24px">
+                        <Radio onChange={onChange} value="Stream">
+                          Stream
+                        </Radio>
+                        <Radio onChange={onChange} value="Batch">
+                          Batch
+                        </Radio>
+                      </HStack>
+                    </RadioGroup>
+                    <FormErrorMessage>{meta.error}</FormErrorMessage>
+                  </FormControl>
+                );
+              }}
+            </Field>
+            {props.values.mode === 'Stream' && (
+              <VStack spacing="1em" align="flex-start">
+                <Field name="stream.streamAddress">
+                  {({ field, meta }) => (
+                    <FormControl
+                      isRequired
+                      isInvalid={meta.touched && meta.error}
+                    >
+                      <FormLabel>Stream Address</FormLabel>
+                      <Input
+                        {...field}
+                        aria-required={false}
+                        placeholder="Stream address"
+                      />
+                      <FormErrorMessage>{meta.error}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field as={Checkbox} name="stream.saveLogs">
+                  Save logs
+                </Field>
+              </VStack>
+            )}
+            {props.values.mode === 'Batch' && (
+              <div>
+                <Field name="batch.numberOfLogs">
+                  {({ field, form, meta }) => (
+                    <FormControl
+                      isRequired
+                      isInvalid={meta.touched && meta.error}
+                    >
+                      <FormLabel>Number of Logs</FormLabel>
+                      <InputGroup maxW="10em">
+                        <NumberInput
+                          min={1}
+                          max={1000000000}
+                          precision={0}
+                          onChange={val => form.setFieldValue(field.name, val)}
+                        >
+                          <NumberInputField placeholder="1000" />
+                        </NumberInput>
+                      </InputGroup>
+                      <FormErrorMessage>{meta.error}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+              </div>
+            )}
             <Button
               mt={4}
               colorScheme="teal"
