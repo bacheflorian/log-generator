657059702fadb149be8fde21eb8d0115a3554e71
LPhoonLee <104865833+LPhoonLee@users.noreply.github.com>
2023-02-04 14:35:12 -0700

    add a message destination for messages sent to the client for a batch mode job

--- src/main/java/com/ad1/loggenerator/controller/LogController.java
+++ src/main/java/com/ad1/loggenerator/controller/LogController.java
@@ -2,11 +2,15 @@ package com.ad1.loggenerator.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ad1.loggenerator.model.LogMessage;
 import com.ad1.loggenerator.model.SelectionModel;
 import com.ad1.loggenerator.service.implementation.BatchService;
 import com.ad1.loggenerator.service.implementation.StreamingService;
@@ -51,4 +55,12 @@ public class LogController {
         streamingService.setContinueStreaming(false);
         return new ResponseEntity<>("Streaming has stopped.", HttpStatus.OK);
     }
+
+    // message destination for batch mode
+    @MessageMapping("/batch/{clientId}")
+    @SendTo("/topic/batch/{clientId}")
+    public LogMessage sendBatchData(LogMessage logMessage, @DestinationVariable int clientId) {
+        return logMessage;
+    }
+
 }
