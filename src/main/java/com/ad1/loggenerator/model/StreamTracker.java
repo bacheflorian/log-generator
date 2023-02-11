65c8cf84283ad4932b3c8094eb59cdb8d066deb8
LPhoonLee <104865833+LPhoonLee@users.noreply.github.com>
2023-02-11 13:45:26 -0700

    Add startTime and endTime
    Add additional variables to StreamTracker object to later be used to calculate statistics

--- src/main/java/com/ad1/loggenerator/model/StreamTracker.java
+++ src/main/java/com/ad1/loggenerator/model/StreamTracker.java
@@ -13,8 +13,13 @@ public class StreamTracker {
     private String jobId;
     // count of log lines generated in stream job
     private int logCount;
-    // time of the last ping from client
+    // time of the last ping from client in seconds
     private long lastPing;
+    // flag to continue streaming or not
     private Boolean continueStreaming;
+    // the start time of the stream job in seconds
+    private long startTime;
+    // the end time of the stream job in seconds
+    private long endTime;
 
 }
