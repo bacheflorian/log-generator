ea2c9385310557de14090cecfbd9f7d29d397927
LPhoonLee <104865833+LPhoonLee@users.noreply.github.com>
2023-03-14 15:30:54 -0600

    add logic to generate logs with user provided values, if not provided then it still randomly generates a value instead

--- backend/src/main/java/com/ad1/loggenerator/service/implementation/LogService.java
+++ backend/src/main/java/com/ad1/loggenerator/service/implementation/LogService.java
@@ -37,34 +37,41 @@ public class LogService {
         JSONObject logLineJSON = new JSONObject();
 
         // Generate log line data
-        if (fieldSettings.isIncludeTimeStamp()) {
-            logLine.setTimeStamp(generateTimeStamp());
+        if (fieldSettings.getTimeStamp().isInclude()) {
+            long values[] = fieldSettings.getTimeStamp().getValues();
+            logLine.setTimeStamp(generateTimeStamp(values));
             logLineJSON.put("timeStamp", logLine.getTimeStamp());
         }
-        if (fieldSettings.isIncludeProcessingTime()) {
-            logLine.setProcessingTime(generateProcessingTime());
+        if (fieldSettings.getProcessingTime().isInclude()) {
+            long values[] = fieldSettings.getProcessingTime().getValues();
+            logLine.setProcessingTime(generateProcessingTime(values));
             logLineJSON.put("processingTime", logLine.getProcessingTime());
         }
-        if (fieldSettings.isIncludeCurrentUserID()) {
-            logLine.setUserId(generateUserId());
+        if (fieldSettings.getCurrentUserID().isInclude()) {
+            String values[] = fieldSettings.getCurrentUserID().getValues();
+            logLine.setUserId(generateUserId(values));
             logLineJSON.put("userId", logLine.getUserId());
         }
-        if (fieldSettings.isIncludeBusinessGUID()) {
-            logLine.setBusinessId(generateBusinessId());
+        if (fieldSettings.getBusinessGUID().isInclude()) {
+            String values[] = fieldSettings.getBusinessGUID().getValues();
+            logLine.setBusinessId(generateBusinessId(values));
             logLineJSON.put("businessId", logLine.getBusinessId());
         }
 
-        if (fieldSettings.isIncludePathToFile()) {
-            logLine.setFilepath(generateFilepath());
+        if (fieldSettings.getPathToFile().isInclude()) {
+            String values[] = fieldSettings.getPathToFile().getValues();
+            logLine.setFilepath(generateFilepath(values));
             logLineJSON.put("filepath", logLine.getFilepath().replace("\\\\", "\\"));
         }
 
-        if (fieldSettings.isIncludeFileSHA256()) {
-            logLine.setFileSHA256(generateFileSHA256());
+        if (fieldSettings.getFileSHA256().isInclude()) {
+            String values[] = fieldSettings.getFileSHA256().getValues();
+            logLine.setFileSHA256(generateFileSHA256(values));
             logLineJSON.put("fileSHA256", logLine.getFileSHA256());
         }
-        if (fieldSettings.isIncludeDisposition()) {
-            logLine.setDisposition(generateDisposition()); // 1 = Clean, 2 = Suspicious, 3 = Malicious, 4 = Unknown
+        if (fieldSettings.getDisposition().isInclude()) {
+            int values[] = fieldSettings.getDisposition().getValues();
+            logLine.setDisposition(generateDisposition(values)); // 1 = Clean, 2 = Suspicious, 3 = Malicious, 4 = Unknown
             logLineJSON.put("disposition", logLine.getDisposition());
         }
 
@@ -78,7 +85,25 @@ public class LogService {
      * 
      * @return a timestamp
      */
-    public long generateTimeStamp() {
+    public long generateTimeStamp(long[] values) {
+        
+        if (values.length == 0) {
+            // if no value provided, generate random timestamp
+            return generateRandomTimeStamp();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random timestamp
+     * 
+     * @return a timestamp
+     */
+    public long generateRandomTimeStamp() {
         return (long) ((System.currentTimeMillis() / 1000) * Math.random());
     }
 
@@ -87,7 +112,25 @@ public class LogService {
      * 
      * @return processing time in seconds
      */
-    public long generateProcessingTime() {
+    public long generateProcessingTime(long[] values) {
+
+        if (values.length == 0) {
+            // if no value provided, generate random processing time
+            return generateRandomProcessingTime();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random processing time
+     * 
+     * @return random processing time in seconds
+     */
+    public long generateRandomProcessingTime() {
         return (long) (Math.random() * 1000);
     }
 
@@ -96,7 +139,25 @@ public class LogService {
      * 
      * @return a unique id a user
      */
-    public String generateUserId() {
+    public String generateUserId(String[] values) {
+
+        if (values.length == 0) {
+            // if no value provided, generate random user id
+            return generateRandomUserId();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random user ID
+     * 
+     * @return a random unique id a user
+     */
+    public String generateRandomUserId() {
         return UUID.randomUUID().toString();
     }
 
@@ -105,7 +166,25 @@ public class LogService {
      * 
      * @return a unique id for a business
      */
-    public String generateBusinessId() {
+    public String generateBusinessId(String[] values) {
+
+        if (values.length == 0) {
+            // if no value provided, generate random business id
+            return generateRandomBusinessId();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random business ID
+     * 
+     * @return a random unique id for a business
+     */
+    public String generateRandomBusinessId() {
         return UUID.randomUUID().toString();
     }
 
@@ -114,7 +193,25 @@ public class LogService {
      * 
      * @return a random filepath
      */
-    public String generateFilepath() {
+    public String generateFilepath(String[] values) {
+
+        if (values.length == 0) {
+            // if no value provided, generate random file path
+            return generateRandomFilepath();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random filepath
+     * 
+     * @return a random filepath
+     */
+    public String generateRandomFilepath() {
         // random information
         String[] folders = { "C:/Program Files", "C:/Windows", "C:/Program Files (x86)",
                 "C:/Program Files (x86)/Common Files", "/tmp", "/home" };
@@ -139,7 +236,25 @@ public class LogService {
      * 
      * @return a file SHA256
      */
-    public String generateFileSHA256() {
+    public String generateFileSHA256(String[] values) {
+
+        if (values.length == 0) {
+            // if no value provided, generate random file sha256
+            return generateRandomFileSHA256();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random file SHA256
+     * 
+     * @return a random file SHA256
+     */
+    public String generateRandomFileSHA256() {
         return UUID.randomUUID().toString();
     }
 
@@ -148,7 +263,25 @@ public class LogService {
      * 
      * @return a disposition
      */
-    public int generateDisposition() {
+    public int generateDisposition(int[] values) {
+
+        if (values.length == 0) {
+            // if no value provided, generate random disposition
+            return generateRandomDisposition();
+        }
+
+        // if value provided, select one at random
+        Random random = new Random();
+        int randomIndex = random.nextInt(values.length);
+        return values[randomIndex];
+    }
+
+    /**
+     * Utility method to generate a random disposition
+     * 
+     * @return a random disposition
+     */
+    public int generateRandomDisposition() {
         return (int) (Math.random() * ((4 - 1) + 1) + 1);
     }
 
