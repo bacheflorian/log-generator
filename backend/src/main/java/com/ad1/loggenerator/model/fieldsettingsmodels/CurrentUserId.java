967cda81cbc3b6ca536001474e598ea3ab4fce7e
LPhoonLee <104865833+LPhoonLee@users.noreply.github.com>
2023-03-14 14:21:20 -0600

    add new models for fieldSettings

---
+++ backend/src/main/java/com/ad1/loggenerator/model/fieldsettingsmodels/CurrentUserId.java
@@ -0,0 +1,17 @@
+package com.ad1.loggenerator.model.fieldsettingsmodels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrentUserId {
+    
+    // whether to include the field
+    private boolean include;
+    // the optional values to include
+    private String[] values;
+    
+}
