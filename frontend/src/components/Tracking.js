a5daaa274a67b6294a050fab26cb699d2246f90c
David Zarinski <david.zarinski@gmail.com>
2023-03-18 12:30:57 -0600

    Changed socket url to read from .env

--- frontend/src/components/Tracking.js
+++ frontend/src/components/Tracking.js
@@ -68,7 +68,7 @@ function Tracking({ jobID, setJobID, startTime, batchMode, batchSize }) {
 
     // connect to socket
     let stompClient = Stomp.over(
-      () => new SockJS('http://localhost:8080/websocket-batch-service')
+      () => new SockJS(process.env.REACT_APP_SOCKET_URL)
     );
     stompClient.debug = () => {}; //disables stomp debug console logs
     stompClient.connect({}, function (frame) {
