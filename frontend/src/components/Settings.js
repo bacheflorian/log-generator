e124fd0b7b04056dc4ba281858abd4b41e48263d
David Zarinski <david.zarinski@gmail.com>
2023-03-18 17:00:34 -0600

    Added custom logs

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,26 +1,54 @@
+import { AddIcon } from '@chakra-ui/icons';
 import {
   Box,
   Button,
   Checkbox,
+  Divider,
   FormControl,
   FormErrorMessage,
   FormHelperText,
   FormLabel,
   HStack,
+  IconButton,
   Input,
   InputGroup,
   InputRightElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   NumberInput,
   NumberInputField,
   Radio,
   RadioGroup,
   Text,
+  useDisclosure,
   VStack,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react';
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
 
+const defaultCustomLog = {
+  frequency: 0.05,
+  fields: {
+    timeStamp: '',
+    processingTime: '',
+    currentUserID: '',
+    businessGUID: '',
+    pathToFile: '',
+    fileSHA256: '',
+    disposition: '',
+  },
+};
+
 function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const validate = values => {
     const errors = {};
 
@@ -58,8 +86,21 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
       errors.mode = 'Invalid mode selected';
     }
 
+    // custom logs validation
+    // validate total frequency less than or equal to 1
+    let total = 0;
+    values.customLogs.forEach(function (customLog) {
+      total += Number(customLog.frequency);
+    });
+
+    if (total > 1) {
+      errors.customLogs =
+        'Frequency of all custom logs must be less than or equal to 1';
+    }
+
     return errors;
   };
+
   const FieldSetting = ({ name, fieldName }) => {
     return (
       <HStack justifyContent="space-between" w="92%">
@@ -94,11 +135,7 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
           fileSHA256: { include: true, values: '' },
           disposition: { include: true, values: '' },
         },
-        malwareSettings: {
-          includeTrojan: false,
-          includeAdware: false,
-          includeRansom: false,
-        },
+        customLogs: [],
         mode: 'Stream',
         streamSettings: {
           streamAddress: '',
@@ -115,9 +152,11 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
           values.batchSettings.numberOfLogs
         );
 
+        // Process body
         // Create a copy of values
         const body = JSON.parse(JSON.stringify(values));
 
+        // field settings
         // split field values
         Object.keys(body.fieldSettings).forEach(function (key) {
           const val = body.fieldSettings[key].values;
@@ -133,6 +172,19 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
             body.fieldSettings[key].values = val.split(',');
           }
         });
+
+        // custom logs
+        // filter empty fields
+        body.customLogs.forEach(function (customLog) {
+          Object.keys(customLog.fields).forEach(
+            key => customLog.fields[key] === '' && delete customLog.fields[key]
+          );
+        });
+
+        // filter customLogs with no fields provided
+        body.customLogs = body.customLogs.filter(
+          customLog => Object.keys(customLog.fields).length !== 0
+        );
 
         // log body
         console.log(JSON.stringify(body, null, 2));
@@ -179,7 +231,7 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
     >
       {props => (
         <Form>
-          <VStack spacing="1.25em" align="flex-start">
+          <VStack spacing="1em" align="flex-start">
             <Field name="repeatingLoglinesPercent">
               {({ field, form }) => (
                 <FormControl isInvalid={form.errors.repeatingLoglinesPercent}>
@@ -222,7 +274,7 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
                 </Text>
                 <Text fontWeight="500">Values</Text>
               </HStack>
-              <VStack spacing="0.5em" align="flex-start">
+              <VStack spacing="0.3em" align="flex-start">
                 <FieldSetting name="Time stamp" fieldName="timeStamp" />
                 <FieldSetting
                   name="Processing time"
@@ -238,20 +290,124 @@ function Settings({ jobID, setJobID, setBatchMode, setBatchSize }) {
                 <FieldSetting name="Disposition" fieldName="disposition" />
               </VStack>
             </FormControl>
-            <FormControl>
-              <FormLabel>Include Malware:</FormLabel>
-              <VStack spacing="0.75em" align="flex-start">
-                <Field as={Checkbox} name="malwareSettings.includeTrojan">
-                  Trojan
-                </Field>
-                <Field as={Checkbox} name="malwareSettings.includeAdware">
-                  Adware
-                </Field>
-                <Field as={Checkbox} name="malwareSettings.includeRansom">
-                  Ransom
-                </Field>
-              </VStack>
-            </FormControl>
+            <Field name="customLogs">
+              {({ field, form, meta }) => (
+                <FormControl isInvalid={meta.error}>
+                  <FormLabel>Custom Logs:</FormLabel>
+                  <VStack align="start">
+                    <HStack spacing="1.5em">
+                      <Text fontWeight="medium">{field.value.length}</Text>
+                      <Button onClick={onOpen} size="sm">
+                        Edit
+                      </Button>
+                    </HStack>
+                    {!isOpen && (
+                      <FormErrorMessage mt="0">{meta.error}</FormErrorMessage>
+                    )}
+                  </VStack>
+                  <Modal
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    size="6xl"
+                    closeOnEsc={false}
+                  >
+                    <ModalOverlay />
+                    <ModalContent>
+                      <ModalHeader>Custom Logs</ModalHeader>
+                      <ModalCloseButton />
+                      <ModalBody>
+                        <VStack align="start">
+                          {field.value.map((log, index) => (
+                            <Wrap
+                              spacingX="1em"
+                              spacingY="0.5em"
+                              key={index}
+                              gap="1em"
+                              pt="0.5em"
+                            >
+                              <WrapItem>
+                                <HStack>
+                                  <Text>frequency:</Text>
+                                  <FormControl isInvalid={meta.error}>
+                                    <InputGroup maxW="8em">
+                                      <NumberInput
+                                        min={0}
+                                        max={1}
+                                        value={log.frequency}
+                                        onChange={val =>
+                                          form.setFieldValue(
+                                            `customLogs.${index}.frequency`,
+                                            val
+                                          )
+                                        }
+                                      >
+                                        <NumberInputField
+                                          h="2em"
+                                          w="6em"
+                                          placeholder="0"
+                                        />
+                                      </NumberInput>
+                                    </InputGroup>
+                                  </FormControl>
+                                </HStack>
+                              </WrapItem>
+                              {Object.keys(log.fields).map(keyName => (
+                                <WrapItem key={`${index}.${keyName}`}>
+                                  <HStack>
+                                    <Text>{keyName}:</Text>
+                                    <FormControl>
+                                      <Input
+                                        defaultValue={log.fields[keyName]}
+                                        //change values onBlur to improve rendering performance
+                                        onBlur={event =>
+                                          form.setFieldValue(
+                                            `customLogs.${index}.fields.${keyName}`,
+                                            event.target.value
+                                          )
+                                        }
+                                        placeholder="random"
+                                        h="2em"
+                                        w="8em"
+                                      />
+                                    </FormControl>
+                                  </HStack>
+                                </WrapItem>
+                              ))}
+                              <Divider pt="0.5em" />
+                            </Wrap>
+                          ))}
+                          <FormErrorMessage>{meta.error}</FormErrorMessage>
+                          <IconButton
+                            icon={<AddIcon />}
+                            size="sm"
+                            onClick={() => {
+                              form.setFieldValue(field.name, [
+                                ...field.value,
+                                defaultCustomLog,
+                              ]);
+                              console.log(field.value);
+                            }}
+                          />
+                        </VStack>
+                      </ModalBody>
+
+                      <ModalFooter>
+                        <Button
+                          variant="ghost"
+                          mr={3}
+                          onClick={() => form.setFieldValue(field.name, [])}
+                        >
+                          Reset
+                        </Button>
+                        <Button colorScheme="blue" onClick={onClose}>
+                          Close
+                        </Button>
+                      </ModalFooter>
+                    </ModalContent>
+                  </Modal>
+                </FormControl>
+              )}
+            </Field>
             <Field name="mode">
               {({ field, meta }) => {
                 const { onChange, ...rest } = field;
