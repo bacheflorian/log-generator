dffdb7a8ffb05b3580a0d7576c44c32e218a51f8
Florian Bache <bacheflorian@gmail.com>
2023-03-23 13:33:28 -0600

    removed setAllowedOrigins

--- backend/src/main/java/com/ad1/loggenerator/config/WebSocketConfig.java
+++ backend/src/main/java/com/ad1/loggenerator/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-batch-service").setAllowedOrigins("${frontend.url}").withSockJS();
+        registry.addEndpoint("/websocket-batch-service");
     }
 
     @Override
