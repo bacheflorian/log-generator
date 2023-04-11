810ffd1e7c1f1eacd5a1432b411a2e4e249458ae
David Zarinski <david.zarinski@gmail.com>
2023-04-11 15:14:43 -0600

    Added /stream/s3/toAddress method

--- backend/src/main/java/com/ad1/loggenerator/service/implementation/AWSStreamService.java
+++ backend/src/main/java/com/ad1/loggenerator/service/implementation/AWSStreamService.java
@@ -3,6 +3,7 @@ package com.ad1.loggenerator.service.implementation;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -10,10 +11,13 @@ import java.util.Set;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.ad1.loggenerator.exception.AWSServiceNotAvailableException;
+import com.ad1.loggenerator.exception.FilePathNotFoundException;
 import com.ad1.loggenerator.model.JobStatus;
 import com.ad1.loggenerator.model.SelectionModel;
 import com.ad1.loggenerator.model.StreamTracker;
@@ -29,6 +33,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import reactor.core.publisher.Mono;
 
 @Getter
 @Setter
@@ -41,6 +46,175 @@ public class AWSStreamService {
     private LogService logService;
     @Autowired
     private AWSLogService awsLogService;
+
+    @Async("asyncTaskExecutor")
+    public void streamToAddress(SelectionModel selectionModel, StreamTracker streamJobTracker)
+            throws InterruptedException {
+
+        // how many logs will be sent in each request
+        int batchSize = 10;
+
+        // times
+        long nsBetweenRequests = (long) (1000000000.0
+                / ((double) selectionModel.getStreamSettings().getLogRate() / batchSize));
+        long nextSendNanoTime = System.nanoTime() + nsBetweenRequests;
+        long nsToNextRequest = 0;
+
+        // array with given batch size
+        JSONObject[] logs = new JSONObject[batchSize];
+
+        // temporary repeating log
+        JSONObject repeatingLog = null;
+
+        // Setup web client for post request to user specified address
+        String streamAddress = selectionModel.getStreamSettings().getStreamAddress();
+        WebClient webClient = WebClient.create(streamAddress);
+        String[] errorMessage = { "" };
+
+        // remove fields that should not be included in custom logs
+        logService.preProcessCustomLogs(selectionModel.getCustomLogs(), selectionModel);
+        Set<String> masterFieldList = logService.getMasterFieldsList(selectionModel.getCustomLogs());
+
+        // should logs be saved
+        boolean saveLogs = selectionModel.getStreamSettings().isSaveLogs();
+
+        // File and FileWriter for savings log lines
+        File tempLogFile = null;
+        FileWriter fileWriter = null;
+
+        try {
+            boolean firstLogSaved = false;
+
+            if (saveLogs) {
+                // FileWriter for savings log lines
+                tempLogFile = new File("logs\\stream\\" + streamJobTracker.getJobId() + ".json");
+                fileWriter = new FileWriter(tempLogFile, true);
+
+                // delete file if it already exists
+                if (tempLogFile.exists()) {
+                    tempLogFile.delete();
+                }
+
+                // Check if the file exists, create a new one if it doesn't exist
+                if (!tempLogFile.exists()) {
+                    tempLogFile.createNewFile();
+                }
+
+                // write a [ to begin the log file
+                fileWriter.write("[");
+            }
+            while (streamJobTracker.getStatus() == JobStatus.ACTIVE) {
+                // generate batchSize number of logs
+                for (int i = 0; i < batchSize; i++) {
+                    // if no repeating log, generate a new log, otherwise add the repeating log
+                    if (repeatingLog == null) {
+                        logs[i] = logService.generateLogLine(selectionModel, masterFieldList);
+
+                        // determine if a log lines repeats
+                        if (Math.random() < selectionModel.getRepeatingLoglinesPercent()) {
+                            repeatingLog = logs[i];
+                        }
+                    } else {
+                        logs[i] = repeatingLog;
+                        repeatingLog = null;
+                    }
+
+                    // writing logs to temp file
+                    if (saveLogs) {
+                        if (firstLogSaved) { // add delimiter if not first log line written
+                            fileWriter.write(",\n");
+                        }
+                        // Write the log lines to the temp log file
+                        fileWriter.write(logs[i].toString());
+
+                        firstLogSaved = true;
+                    }
+                }
+                // nanoseconds until next request time
+                nsToNextRequest = nextSendNanoTime - System.nanoTime();
+
+                // sleep for nsToNextRequest
+                if (nsToNextRequest >= 0) {
+                    // sleep time is subject to precision of system schedulers
+                    Thread.sleep(nsToNextRequest / 1000000, (int) nsToNextRequest % 1000000);
+
+                    // update next send time
+                    nextSendNanoTime += nsBetweenRequests;
+                } else if (nsToNextRequest >= -100000000) {
+                    // if nsToNextRequest is behind but within 100ms, attempt to catch up next
+                    // request
+                    nextSendNanoTime += nsBetweenRequests;
+                } else {
+                    // if nsToNextRequest is behind by more than 100ms, reset nextSendNanoTime
+                    nextSendNanoTime = System.nanoTime() + nsBetweenRequests;
+                }
+
+                // set up post request
+                Mono<String> response = webClient.post()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(logs)
+                        .retrieve()
+                        .bodyToMono(String.class);
+
+                // initiate post request and receive response from address
+                response.subscribe(
+                        res -> {
+                            // System.out.println("Successful response: " + res);
+                        },
+                        error -> {
+                            streamJobTracker.setStatus(JobStatus.FAILED);
+                            errorMessage[0] = "Error while making the request: " + error;
+                            System.out.println(error);
+                        });
+
+                // increment log count by batch size
+                streamJobTracker.setLogCount(streamJobTracker.getLogCount() + batchSize);
+            }
+
+            // upload logs to s3 is saveLogs
+            if (saveLogs) {
+                // write a ] to end the log file
+                fileWriter.write("]");
+                fileWriter.close();
+
+                // upload logs to s3
+                try {
+                    saveLogsToAWSS3(streamJobTracker);
+                } catch (AWSServiceNotAvailableException e) {
+                    // Mark the job as failed if exception occurred
+                    streamJobTracker.setStatus(JobStatus.FAILED);
+
+                    throw new AWSServiceNotAvailableException(e.getMessage());
+                } catch (RuntimeException e) {
+                    // Mark the job as failed if exception occurred
+                    streamJobTracker.setStatus(JobStatus.FAILED);
+
+                    throw new RuntimeException(e.getMessage());
+                } finally {
+                    // delete temp file if exists
+                    if (tempLogFile.exists()) {
+                        tempLogFile.delete();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            // Mark the job as failed if exception occurred
+            streamJobTracker.setStatus(JobStatus.FAILED);
+
+            // delete temp file if exists
+            if (tempLogFile != null && tempLogFile.exists()) {
+                tempLogFile.delete();
+            }
+            throw new FilePathNotFoundException(e.getMessage());
+        }
+
+        // Mark the job as completed if no errors, failed otherwise
+        if (errorMessage[0].isEmpty()) {
+            streamJobTracker.setStatus(JobStatus.COMPLETED);
+        } else {
+            streamJobTracker.setStatus(JobStatus.FAILED);
+        }
+    }
 
     @Async("asyncTaskExecutor")
     public void streamToS3Buffer(SelectionModel selectionModel, StreamTracker streamJobTracker)
@@ -214,7 +388,7 @@ public class AWSStreamService {
         // create s3 client instance
         AmazonS3 s3Client = awsLogService.createS3Client();
         // specify the local file path to upload
-        File tempLogFile = new File(streamJobTracker.getJobId() + ".json");
+        File tempLogFile = new File("logs\\stream\\" + streamJobTracker.getJobId() + ".json");
         if (!tempLogFile.exists()) {
             throw new RuntimeException("temp log file do not exist");
         }
